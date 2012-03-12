1434f6549d6e1a33efffc385bb1bd59a56f1227e
Jordan Hatch <jordan@jordanh.net>
2012-03-12 14:37:30 +0000

    Reordered questions and added some copy from Donna

--- lib/flows/report-a-lost-or-stolen-passport.rb
+++ lib/flows/report-a-lost-or-stolen-passport.rb
@@ -1,21 +1,41 @@
 status :draft
 
 multiple_choice :has_your_passport_been_lost_or_stolen? do
-  option :lost => :where_was_your_passport_lost?
-  option :stolen => :where_was_your_passport_stolen?
+  option :lost
+  option :stolen
+
+  next_node :adult_or_child_passport?
 
   save_input_as :lost_or_stolen
 end
 
+multiple_choice :adult_or_child_passport? do
+  option :adult
+  option :child
+
+  save_input_as :age
+
+  calculate :child_advice do 
+    age == 'child' ? PhraseList.new(:child_forms) : PhraseList.new
+  end
+
+  next_node do 
+    case lost_or_stolen
+      when 'lost' then :where_was_your_passport_lost?
+      when 'stolen' then :where_was_your_passport_stolen?
+    end
+  end
+end
+
 multiple_choice :where_was_your_passport_stolen? do
-  option :in_the_uk => :adult_or_child_passport?
+  option :in_the_uk => :contact_the_police
   option :abroad => :which_country?
 
   save_input_as :location
 end
 
 multiple_choice :where_was_your_passport_lost? do
-  option :in_the_uk => :adult_or_child_passport?
+  option :in_the_uk => :complete_LS01_form
   option :abroad => :which_country?
 
   save_input_as :location
@@ -36,18 +56,6 @@ country_select :which_country? do
   end
 
   next_node :contact_the_embassy
-end
-
-multiple_choice :adult_or_child_passport? do
-  option :adult
-  option :child
-
-  next_node do
-    case lost_or_stolen
-      when 'lost' then :complete_LS01_form
-      when 'stolen' then :contact_the_police
-    end
-  end
 end
 
 outcome :contact_the_police do
